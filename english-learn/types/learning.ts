6d331026f0fde16fe594d85da5aa1de207b370d4
Tianbo Cao <2617401@dundee.ac.uk>
2026-03-19 20:11:16 +0800

    improved speaking part

--- english-learn/types/learning.ts
+++ english-learn/types/learning.ts
@@ -39,6 +39,8 @@ export interface SpeakingFeedback {
   grammar_score: number;
   strengths: string[];
   revision_focus: string;
+  delivery_snapshot: string;
+  sample_upgrade: string;
   tips: string[];
 }
 
