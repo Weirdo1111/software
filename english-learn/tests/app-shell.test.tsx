5adfe9319664b42597ef8d199b1364e743335b9c
Chengzeng-Zane <chengzeng0530@gmail.com>
2026-04-07 14:53:38 +0800

    test(app-shell): align challenge navigation assertion with current route

--- english-learn/tests/app-shell.test.tsx
+++ english-learn/tests/app-shell.test.tsx
@@ -61,6 +61,6 @@ describe("AppShell", () => {
 
     await user.click(screen.getByRole("button", { name: /challenge/i }));
 
-    expect(pushSpy).toHaveBeenCalledWith("/listening/test?lang=en");
+    expect(pushSpy).toHaveBeenCalledWith("/challenge?lang=en");
   });
 });
